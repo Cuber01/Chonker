e429d1d55e7d59a7482c759513c4804309d2dad4
Cuber01 <johnmallard631@gmail.com>
2022-03-23 15:33:40 +0100

    finished tests

--- Chonker/Tests/run_tests.py
+++ Chonker/Tests/run_tests.py
@@ -1,6 +1,15 @@
 import os
 import subprocess
+import time
 from pathlib import Path
+
+tests_total = 0
+tests_passed = 0
+
+ansii_reset = "\033[0m"
+ansii_bold = "\033[1m"
+ansii_failure = "\033[91m"
+ansii_success = "\033[92m"
 
 path_to_exec = os.path.join("..", "bin", "Debug", "net6.0", "Chonker")
 
@@ -12,9 +21,12 @@ path_to_bad_tests = os.path.join("Error", "")
 def run_tests():
     run_good_tests()
     run_error_tests()
+    print_score()
 
 
 def run_good_tests():
+    print(ansii_bold + "Main:" + ansii_reset)
+
     tests = os.listdir(path_to_good_tests)
     outputs = os.listdir(path_to_output)
 
@@ -28,11 +40,15 @@ def run_good_tests():
 
     # Run tests
     for test in tests:
+        time_started = time.time()
+
         child = subprocess.Popen([path_to_exec, test], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         child.wait()
 
+        execution_time = time.time() - time_started
+
         if child.returncode == 1:
-            print("Failure")
+            submit_result(Path(test).stem, execution_time, False)
         else:
             for output in outputs:
                 if Path(output).stem in test:
@@ -40,23 +56,29 @@ def run_good_tests():
                     contents = f.read()
 
                     if contents == child.communicate()[0].decode("utf-8"):
-                        print("Success")
+                        submit_result(Path(test).stem, execution_time, True)
                     else:
-                        print("Failure")
+                        submit_result(Path(test).stem, execution_time, False)
 
 
 def run_error_tests():
+    print(ansii_bold + "\nError Catch:" + ansii_reset)
+
     tests = os.listdir(path_to_bad_tests)
     tests = get_full_paths(tests, path_to_bad_tests)
 
     for test in tests:
+        time_started = time.time()
+
         child = subprocess.Popen([path_to_exec, test], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         child.wait()
 
+        execution_time = time.time() - time_started
+
         if child.returncode == 1:
-            print("Success")
+            submit_result(Path(test).stem, execution_time, True)
         else:
-            print("Failure")
+            submit_result(Path(test).stem, execution_time, False)
 
 
 def get_full_paths(paths, additional_path):
@@ -65,6 +87,37 @@ def get_full_paths(paths, additional_path):
         tmp.append(os.getcwd() + os.sep + additional_path + filepath)
 
     return tmp
+
+
+def submit_result(file, execution_time, success):
+    print(file, end='')
+
+    global tests_total
+    global tests_passed
+    tests_total += 1
+
+    if success:
+        tests_passed += 1
+
+        print(ansii_success, end='')
+        print(" " + str(round(execution_time, 2)) + "s", end='')
+        print(" ✔")
+    else:
+        print(ansii_failure, end='')
+        print(" " + str(round(execution_time, 2)) + "s", end='')
+        print(" ✘")
+
+    print(ansii_reset, end='')
+
+
+def print_score():
+
+    if tests_passed > (tests_total / 2):
+        print(ansii_success)
+    else:
+        print(ansii_failure)
+
+    print(str(tests_passed) + "/" + str(tests_total) + " tests passed")
 
 
 run_tests()
